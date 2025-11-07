a98ced7a42f1573eaba44253dbfbc3648cf7b2b8
川口嵐士 <arashi@kawaguchiarashishinoMacBook-Pro.local>
2025-11-07 13:28:16 +0900

    fix: vl_test

--- setup.sh
+++ setup.sh
@@ -24,33 +24,44 @@ function vl_test () {
   if [ "$1" = "answer" ]; then
     test_dir=test_answer
   fi
-  _vl_test_subject $test_dir/test1/main.vl "'Matcher株式会社' を出力しなさい"
-  _vl_test_case $test_dir/test1/main.vl "" "Matcher株式会社"
-  echo ""
 
-  _vl_test_subject $test_dir/test2/main.vl "入力の2乗を出力しなさい"
-  _vl_test_case $test_dir/test2/main.vl "3" "9"
-  _vl_test_case $test_dir/test2/main.vl "0" "0"
-  _vl_test_case $test_dir/test2/main.vl "-2" "4"
-  echo ""
+  if [ "$1" = "1" ] || [ "$1" = "" ]; then
+    _vl_test_subject $test_dir/test1/main.vl "'Matcher株式会社' を出力しなさい"
+    _vl_test_case $test_dir/test1/main.vl "" "Matcher株式会社"
+    echo ""
+  fi
 
-  _vl_test_subject $test_dir/test3/main.vl "入力の名前を使って'私の名前は<name>です'を出力しなさい"
-  _vl_test_case $test_dir/test3/main.vl "山田太郎" "私の名前は山田太郎です"
-  _vl_test_case $test_dir/test3/main.vl "抹茶花子" "私の名前は抹茶花子です"
-  echo ""
+  if [ "$1" = "2" ] || [ "$1" = "" ]; then
+    _vl_test_subject $test_dir/test2/main.vl "入力の2乗を出力しなさい"
+    _vl_test_case $test_dir/test2/main.vl "3" "9"
+    _vl_test_case $test_dir/test2/main.vl "0" "0"
+    _vl_test_case $test_dir/test2/main.vl "-2" "4"
+    echo ""
+  fi
 
-  _vl_test_subject $test_dir/test4/main.vl "入力が'りんご'の場合は'apple'、'ばなな'の場合は'banana'、それ以外の場合は'fruit'を出力しなさい"
-  _vl_test_case $test_dir/test4/main.vl "りんご" "apple"
-  _vl_test_case $test_dir/test4/main.vl "ばなな" "banana"
-  _vl_test_case $test_dir/test4/main.vl "ぶどう" "fruit"
-  _vl_test_case $test_dir/test4/main.vl "れもん" "fruit"
-  echo ""
+  if [ "$1" = "3" ] || [ "$1" = "" ]; then
+    _vl_test_subject $test_dir/test3/main.vl "入力の名前を使って'私の名前は<name>です'を出力しなさい"
+    _vl_test_case $test_dir/test3/main.vl "山田太郎" "私の名前は山田太郎です"
+    _vl_test_case $test_dir/test3/main.vl "抹茶花子" "私の名前は抹茶花子です"
+    echo ""
+  fi
 
-  _vl_test_subject $test_dir/test5/main.vl "入力の階乗を出力しなさい"
-  _vl_test_case $test_dir/test5/main.vl "0" "1"
-  _vl_test_case $test_dir/test5/main.vl "1" "1"
-  _vl_test_case $test_dir/test5/main.vl "4" "24"
-  echo ""
+  if [ "$1" = "4" ] || [ "$1" = "" ]; then
+    _vl_test_subject $test_dir/test4/main.vl "入力が'りんご'の場合は'apple'、'ばなな'の場合は'banana'、それ以外の場合は'fruit'を出力しなさい"
+    _vl_test_case $test_dir/test4/main.vl "りんご" "apple"
+    _vl_test_case $test_dir/test4/main.vl "ばなな" "banana"
+    _vl_test_case $test_dir/test4/main.vl "ぶどう" "fruit"
+    _vl_test_case $test_dir/test4/main.vl "れもん" "fruit"
+    echo ""
+  fi
+
+  if [ "$1" = "5" ] || [ "$1" = "" ]; then
+    _vl_test_subject $test_dir/test5/main.vl "入力の階乗を出力しなさい"
+    _vl_test_case $test_dir/test5/main.vl "0" "1"
+    _vl_test_case $test_dir/test5/main.vl "1" "1"
+    _vl_test_case $test_dir/test5/main.vl "4" "24"
+    echo ""
+  fi
 }
 
 function _vl_test_subject () {
